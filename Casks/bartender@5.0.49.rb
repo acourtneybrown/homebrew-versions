ecf5c59624b8a9e30e32fdc5e37ea2aaff9f8065
Adam Brown <acourtneybrown@users.noreply.github.com>
2025-11-05 16:09:41 -0700

    Update GitHub Actions versions (#2)
    * Update `actions/cache` to v4
    
    * Ran `brew style --fix .`
    
    * Remove Python 3.6 formula
    
    * Fix shellcheck errors
    
    * Fix version of bartender in cask file

--- Casks/bartender@5.0.49.rb
+++ Casks/bartender@5.0.49.rb
@@ -1,12 +1,6 @@
 cask "bartender@5.0.49" do
-  on_ventura :or_older do
-    version "4.2.25"
-    sha256 "f1a2ecf300cf56aaf531324cba646442026cb0b63b4f90f46a1aee5b0804688a"
-  end
-  on_sonoma :or_newer do
-    version "5.0.49"
-    sha256 "7f91ad8d0aee86f5c784096d0b3d732f518356d3e1b2b3b9db2e196a0740574b"
-  end
+  version "5.0.49"
+  sha256 "7f91ad8d0aee86f5c784096d0b3d732f518356d3e1b2b3b9db2e196a0740574b"
 
   url "https://macbartender.com/B2/updates/#{version.dots_to_hyphens}/Bartender%20#{version.major}.zip"
   name "Bartender"
@@ -26,14 +20,14 @@ cask "bartender@5.0.49" do
 
   app "Bartender #{version.major}.app"
 
-  uninstall delete:    [
+  uninstall launchctl: "com.surteesstudios.Bartender.BartenderInstallHelper",
+            quit:      "com.surteesstudios.Bartender",
+            delete:    [
               "/Library/Audio/Plug-Ins/HAL/BartenderAudioPlugIn.plugin",
               "/Library/PrivilegedHelperTools/com.surteesstudios.Bartender.BartenderInstallHelper",
               "/Library/ScriptingAdditions/BartenderHelper.osax",
               "/System/Library/ScriptingAdditions/BartenderSystemHelper.osax",
-            ],
-            launchctl: "com.surteesstudios.Bartender.BartenderInstallHelper",
-            quit:      "com.surteesstudios.Bartender"
+            ]
 
   zap trash: [
     "~/Library/Caches/com.surteesstudios.Bartender",
